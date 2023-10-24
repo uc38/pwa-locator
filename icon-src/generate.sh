8988dacb1c786dcafe5e7edde31ea725d87f5178
Gerhard Schwaerzler <gerhard.schwaerzler@nu-gmbh.com>
2023-10-24 11:11:27 +0200

    full screen map; homescreen icons

---
+++ icon-src/generate.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+inkscape -w 72 -h 72 icon.svg -o ../src/icons/locator_72x72.png
+inkscape -w 96 -h 96 icon.svg -o ../src/icons/locator_96x96.png
+inkscape -w 120 -h 120 icon.svg -o ../src/icons/locator_120x120.png
+inkscape -w 128 -h 128 icon.svg -o ../src/icons/locator_128x128.png
+inkscape -w 144 -h 144 icon.svg -o ../src/icons/locator_144x144.png
+inkscape -w 152 -h 152 icon.svg -o ../src/icons/locator_152x152.png
+inkscape -w 180 -h 180 icon.svg -o ../src/icons/locator_180x180.png
+inkscape -w 192 -h 192 icon.svg -o ../src/icons/locator_192x192.png
+inkscape -w 384 -h 384 icon.svg -o ../src/icons/locator_384x384.png
+inkscape -w 512 -h 512 icon.svg -o ../src/icons/locator_512x512.png
--- src/locator.js
+++ src/locator.js
@@ -1,19 +1,28 @@
+import cameraImage from './camera.svg';
+
 const COORD_FORMATTER = Intl.NumberFormat('de-DE', { minimumFractionDigits: 6, maximumFractionDigits: 6, minimumIntegerDigits: 3, style: 'unit', unit: 'degree' });
 const DIST_FORMATTER = Intl.NumberFormat('de-DE', { minimumFractionDigits: 1, maximumFractionDigits: 1, style: 'unit', unit: 'meter' });
 const DEG_FORMATTER = Intl.NumberFormat('de-DE', { minimumFractionDigits: 1, maximumFractionDigits: 1, style: 'unit', unit: 'degree' });
 
-const LOCATOR_ID = 'location';
+const LOCATION_ID = 'location';
 const CAMERA_INPUT_ID = 'camera';
-
-const locatorDiv = document.getElementById(LOCATOR_ID);
-const cameraButton = document.getElementById(CAMERA_INPUT_ID);
 
 //map state
 var map;
 var ranger;
 
+function isTouchDevice() {
+    return (('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0) ||
+        (navigator.msMaxTouchPoints > 0));
+}
+
 function configureMap(latLngArray) {
     map = L.map('map').setView(latLngArray, 17);
+    if (isTouchDevice()) {
+        map.removeControl(map.zoomControl);
+    }
+    map.attributionControl.setPosition('bottomleft');
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -22,24 +31,24 @@ function configureMap(latLngArray) {
     ranger = L.circle(latLngArray, { radius: 20.0 }).addTo(map);
 }
 
-let currentCoords = null;
-
 function updatePosition(position) {
+    const locatorDiv = document.getElementById(LOCATION_ID);
+
     const coords = position.coords;
     console.debug(`got new coordinates: ${coords}`);
     locatorDiv.innerHTML = `
         <dl>
-            <dt>lat</dt>
+            <dt>LAT</dt>
             <dd>${COORD_FORMATTER.format(coords.latitude)}</dd>
-            <dt>long</dt>
+            <dt>LONG</dt>
             <dd>${COORD_FORMATTER.format(coords.longitude)}</dd>
-            <dt>alt</dt>
+            <dt>ALT</dt>
             <dd>${coords.altitude ? DIST_FORMATTER.format(coords.altitude) : '-'}</dd>
-            <dt>accuracy</dt>
+            <dt>ACC</dt>
             <dd>${DIST_FORMATTER.format(coords.accuracy)}</dd>
-            <dt>heading</dt>
+            <dt>HEAD</dt>
             <dd>${coords.heading ? DEG_FORMATTER.format(coords.heading) : '-'}</dd>
-            <dt>speed</dt>
+            <dt>SPED</dt>
             <dd>${coords.speed ? DIST_FORMATTER.format(coords.speed) : '-'}</dd>
         </dl>
     `;
@@ -51,9 +60,16 @@ function updatePosition(position) {
     ranger.setRadius(coords.accuracy);
 }
 
-function logError(err) {
-    console.error(err.message);
-}
-
 /* setup component */
-configureMap([47.406653, 9.744844]);
+window.onload = () => {
+    const cameraButton = document.getElementById(CAMERA_INPUT_ID);
+
+    //setup UI
+    cameraButton.src = cameraImage;
+
+    //init leaflet
+    configureMap([47.406653, 9.744844]);
+
+    //init footer
+    updatePosition({ coords: { latitude: 47.406653, longitude: 9.744844, altitude: 440, accuracy: 40, heading: 45, speed: 1.8 } });
+}
